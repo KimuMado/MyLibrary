e1036bbffa699abaf01e45ed3a1c7e45a7567fb0
Madoka Kimura <madoka_kimura@otolympuscojp.onmicrosoft.com>
2022-08-31 16:34:59 +0900

    BookとAuthorのMVCを作成

---
+++ MyLibrary/MyLibrary/Migrations/20220830094747_initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyLibrary.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Subtitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LeadSentence = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ MyLibrary/MyLibrary/Models/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Models
+{
+    public class Book
+    {
+        //Primary key
+        public int BookId { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Subtitle { get; set; }
+
+        public string LeadSentence { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        //カバー画像のURL
+        public string ImageUrl { get; set; }
+
+        //著者（複数の場合もある）
+        public ICollection<Author> Authors { get; set; }
+
+        //出版レーベル
+        public Publisher Publisher { get; set; }
+    }
+
+    public class Author
+    {
+        //Primary key
+        public int AuthorId { get; set; }
+
+        public string Name { get; set; }
+        
+        //著書
+        public ICollection<Book> Books { get; set; }
+    }
+
+    public class Publisher
+    {
+        //Primary key
+        public int PublisherId { get; set; }
+
+        public string Label { get; set; }
+
+        //出版書籍
+        public ICollection<Book> Books { get; set; }
+    }
+}
