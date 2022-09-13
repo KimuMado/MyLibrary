286c972ffca2ac9eeb92014a44f3bcea7f0870ce
Madoka Kimura <madoka_kimura@otolympuscojp.onmicrosoft.com>
2022-09-13 21:10:33 +0900

    著者を1人に変更

---
+++ MyLibraryRazor/MyLibraryRazor/Migrations/20220913120810_deletebookauthors.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyLibraryRazor.Migrations
+{
+    public partial class deletebookauthors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Author_Book_BookId",
+                table: "Author");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Publisher_PublisherId",
+                table: "Book");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Author_BookId",
+                table: "Author");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Author");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PublisherId",
+                table: "Book",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Book",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_AuthorId",
+                table: "Book",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                table: "Book",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Publisher_PublisherId",
+                table: "Book",
+                column: "PublisherId",
+                principalTable: "Publisher",
+                principalColumn: "PublisherId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                table: "Book");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Publisher_PublisherId",
+                table: "Book");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Book_AuthorId",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PublisherId",
+                table: "Book",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "Author",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Author_BookId",
+                table: "Author",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Author_Book_BookId",
+                table: "Author",
+                column: "BookId",
+                principalTable: "Book",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Publisher_PublisherId",
+                table: "Book",
+                column: "PublisherId",
+                principalTable: "Publisher",
+                principalColumn: "PublisherId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- MyLibraryRazor/MyLibraryRazor/Pages/Create.cshtml.cs
+++ MyLibraryRazor/MyLibraryRazor/Pages/Create.cshtml.cs
@@ -29,8 +29,6 @@ namespace MyLibraryRazor.Pages
         [BindProperty]
         public Book Book { get; set; }
 
-        public IList<Author> Authors { get; set; }
-
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
